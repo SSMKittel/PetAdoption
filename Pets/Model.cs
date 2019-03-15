d637b70aa9c9125dde78deaddfd29f2b7ebc73b0
Sam Kittel <Sam Kittel@localhost>
2019-03-15 16:50:02 +1030

    Filtering checkboxes bound to data context

---
+++ Pets/Model.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Pets
+{
+    internal class Model
+    {
+        public bool Cat { get; set; } = true;
+        public bool Dog { get; set; } = true;
+        public bool Rabbit { get; set; } = true;
+        public bool Other { get; set; } = true;
+    }
+}
